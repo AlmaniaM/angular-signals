55cf2e6a728c97185d84937ffb2a203bc03d7dbc
AlmaniaM <8919797+AlmaniaM@users.noreply.github.com>
2023-12-29 12:05:36 -0800

    working on last chart reandomize function

---
+++ src/app/chart-options/bar-chart.ts
@@ -0,0 +1,17 @@
+import { EChartsOption } from 'echarts';
+
+export const createBarChartOptions: () => EChartsOption = () => ({
+  title: {
+    text: 'Bar Chart Example'
+  },
+  tooltip: {},
+  xAxis: {
+    data: ["Category A", "Category B", "Category C", "Category D", "Category E"]
+  },
+  yAxis: {},
+  series: [{
+    name: 'Sales',
+    type: 'bar',
+    data: [50, 200, 360, 100, 300]
+  }]
+});
